85fa1913af7862a5e16093b67282095b0869d591
DiegoSF9009 <luminoz0feria@gmail.com>
2026-05-27 20:28:06 -0600

    CLASE 6

--- Assets/Scrips/SoungManager.cs
+++ Assets/Scrips/SoungManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 public class SoungManager : MonoBehaviour
 {
      [SerializeField]
@@ -12,6 +13,8 @@ public class SoungManager : MonoBehaviour
      private UnityEvent onSongEnd;
      [SerializeField]
      private NotesManager notesManager;
+     [SerializeField]
+     private string failAnimationName = "Hit";
      private SoundData currentSongData;
      public void SelectSong(SoundData soundData)
     {
@@ -29,6 +32,28 @@ public class SoungManager : MonoBehaviour
         SoundManager.instance.PlayMusic(currentSongData.songName);
         notesManager.StartNoteChart(currentSongData.noteChart, currentSongData.speed);
         onSongStart?.Invoke();
+    }
+    public void Fail()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FailCoroutine());
+    }
+    private IEnumerator FailCoroutine()
+    {
+        characterAnimator.Play(failAnimationName, 0, 0f);
+        yield return null;
+        yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
+        characterAnimator.Play(currentSongData.animationName, 0, 0f);
+    }
+    public void WinSong()
+    {
+        StopAllCoroutines();
+        characterAnimator.Play("Win", 0, 0f);
+    }
+    public void LoseSong()
+    {
+        StopAllCoroutines();
+        characterAnimator.Play("Lose", 0, 0f);
     }
 
 }
